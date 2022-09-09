adf5dd0f2f45d21d01571959f03053725fe8f5c2
DandyCreater <dandyvialpando@gmail.com>
2022-09-09 16:27:40 +0700

    Update FFI

--- lib/decrypt_ffi.dart
+++ lib/decrypt_ffi.dart
@@ -5,33 +5,51 @@ import 'package:ffi/ffi.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class DecryptFFI extends StatefulWidget {
-  final String? input;
-  const DecryptFFI({super.key, required this.input});
-
-  @override
-  State<DecryptFFI> createState() => _DecryptFFIState();
-}
-
-class _DecryptFFIState extends State<DecryptFFI> {
+class DecryptFFI{
   String stringresult = "";
   final DynamicLibrary trialFFILib = Platform.isAndroid
       ? DynamicLibrary.open('libtrial_ffi.so')
       : DynamicLibrary.process();
 
-   tryDecrypt(Pointer<Utf8> inputs) {
+  tryEncrypt(Pointer<Utf8> inputs) {
     final Pointer<Utf8> Function(Pointer<Utf8> text) trialFFI = trialFFILib
         .lookup<NativeFunction<Pointer<Utf8> Function(Pointer<Utf8>)>>(
             'decrypt')
         .asFunction();
-
     stringresult = trialFFI(inputs).toDartString();
     print(stringresult);
     return stringresult;
   }
-  
-  @override
-  Widget build(BuildContext context) {
-    return tryDecrypt(widget.input!.toNativeUtf8());
-  }
 }
+
+
+// class DecryptFFI extends StatefulWidget {
+//   final String? input;
+//   const DecryptFFI({super.key, required this.input});
+
+//   @override
+//   State<DecryptFFI> createState() => _DecryptFFIState();
+// }
+
+// class _DecryptFFIState extends State<DecryptFFI> {
+//   String stringresult = "";
+//   final DynamicLibrary trialFFILib = Platform.isAndroid
+//       ? DynamicLibrary.open('libtrial_ffi.so')
+//       : DynamicLibrary.process();
+
+//    tryDecrypt(Pointer<Utf8> inputs) {
+//     final Pointer<Utf8> Function(Pointer<Utf8> text) trialFFI = trialFFILib
+//         .lookup<NativeFunction<Pointer<Utf8> Function(Pointer<Utf8>)>>(
+//             'decrypt')
+//         .asFunction();
+
+//     stringresult = trialFFI(inputs).toDartString();
+//     print(stringresult);
+//     return stringresult;
+//   }
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return tryDecrypt(widget.input!.toNativeUtf8());
+//   }
+// }
--- lib/encrypt_ffi.dart
+++ lib/encrypt_ffi.dart
@@ -5,16 +5,8 @@ import 'package:ffi/ffi.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class EncryptFFI extends StatefulWidget {
-  final String? input;
-  const EncryptFFI({super.key, required this.input});
-
-  @override
-  State<EncryptFFI> createState() => _EncryptFFIState();
-}
-
-class _EncryptFFIState extends State<EncryptFFI> {
-  String stringresult = "";
+class EncryptFFI{
+   String stringresult = "";
   final DynamicLibrary trialFFILib = Platform.isAndroid
       ? DynamicLibrary.open('libtrial_ffi.so')
       : DynamicLibrary.process();
@@ -28,9 +20,36 @@ class _EncryptFFIState extends State<EncryptFFI> {
     print(stringresult);
     return stringresult;
   }
-  
-  @override
-  Widget build(BuildContext context) {
-    return tryEncrypt(widget.input!.toNativeUtf8());
-  }
+
 }
+// class EncryptFFI extends StatefulWidget {
+//   final String? input;
+//   const EncryptFFI({super.key, required this.input});
+
+//   @override
+//   State<EncryptFFI> createState() => _EncryptFFIState();
+// }
+
+// class _EncryptFFIState extends State<EncryptFFI> {
+
+
+//   String stringresult = "";
+//   final DynamicLibrary trialFFILib = Platform.isAndroid
+//       ? DynamicLibrary.open('libtrial_ffi.so')
+//       : DynamicLibrary.process();
+
+//   tryEncrypt(Pointer<Utf8> inputs) {
+//     final Pointer<Utf8> Function(Pointer<Utf8> text) trialFFI = trialFFILib
+//         .lookup<NativeFunction<Pointer<Utf8> Function(Pointer<Utf8>)>>(
+//             'encrypt')
+//         .asFunction();
+//     stringresult = trialFFI(inputs).toDartString();
+//     print(stringresult);
+//     return stringresult;
+//   }
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return tryEncrypt(widget.input!.toNativeUtf8());
+//   }
+// }
